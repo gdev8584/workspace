1c712c7a45ef61a047dfcdaabd435f4f0debeeee
Devanand Giri <102689281+gdev8584@users.noreply.github.com>
2025-04-13 07:54:51 +0000

    fi added test1

--- index.ts
+++ index.ts
@@ -5,10 +5,12 @@ const port = 3000;
 import { fileReadAsync } from "./nodejs/fileReadAsync";
 import { fsPromise } from "./nodejs/fsPromisesModule";
 import { eventModule } from "./nodejs/eventModules";
+import { test1 } from "./nodejs/nodejs-task-1";
 
 // fileReadAsync();
 // fsPromise();
-eventModule();
+// eventModule();
+test1();
 // app.get("/", (req, res) => {
 //   res.send("Hello CodeSandbox!");
 // });
--- nodejs/nodejs-task-1.ts
+++ nodejs/nodejs-task-1.ts
@@ -1,7 +1,7 @@
 //? challenges: "Event Maestro: Handle It All"
 
 //!objective
-//* create program using NOde.js EventEmiiter that:
+//* create program using Node.js EventEmiter that:
 
 //? Listens for multiple typesof user events(eg. login, logout, purches and profile-update)
 //? track how many time each event emitted
@@ -12,3 +12,68 @@
 //? emit multiple time with diffrent arguments(eg. userName, item purchage)
 //? Track and store of each event types
 //? define summary of each event was triggered
+
+import EventEmitter from "events";
+import fs from "fs";
+import path from "path";
+const emitter = new EventEmitter();
+
+export function test1() {
+  const filePath = path.join(__dirname, "summary-count.txt");
+
+  const countEmit = () => {
+    if (fs.existsSync(filePath)) {
+      const data = fs.readFileSync(filePath, "utf8");
+      return JSON.parse(data);
+    }
+    return {
+      "user-login": 0,
+      "user-logout": 0,
+      "user-purchase": 0,
+      "profile-update": 0,
+    };
+  };
+
+  const loadCountEmitter = countEmit();
+
+  const saveEmitterCount = (counts: any) => {
+    fs.writeFileSync(filePath, JSON.stringify(counts, null, 2), "utf8"); // null,2 is used for pretty jsom not more
+  };
+  emitter.on("user-login", (userName) => {
+    loadCountEmitter["user-login"]++;
+    console.log(`user is now login ${userName}`);
+    saveEmitterCount(loadCountEmitter);
+  });
+
+  emitter.on("user-purchase", (userName, item) => {
+    loadCountEmitter["user-purchase"]++;
+    console.log(`${userName} is purchase ${item}`);
+    saveEmitterCount(loadCountEmitter);
+  });
+
+  emitter.on("profile-update", (userName, list) => {
+    loadCountEmitter["profile-update"]++;
+    console.log(`${userName} is update profile ${list}}`);
+    saveEmitterCount(loadCountEmitter);
+  });
+
+  emitter.on("user-logout", (userName) => {
+    loadCountEmitter["user-logout"]++;
+    console.log(`user is now logout ${userName}`);
+    saveEmitterCount(loadCountEmitter);
+  });
+
+  emitter.on("summary", () => {
+    console.log("\n event summary");
+    for (const [event, count] of Object.entries(loadCountEmitter)) {
+      console.log(`${event}: ${count}`);
+    }
+  });
+
+  emitter.emit("user-login", "Devanand");
+  emitter.emit("user-purchase", "Devanand", "Laptop");
+  emitter.emit("profile-update", "Devanand", "email");
+  emitter.emit("user-logout", "Devanand");
+
+  emitter.emit("summary");
+}
